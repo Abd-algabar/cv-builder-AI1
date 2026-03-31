8a46a9ea07fcfe35bf3a7a2a37faa8335bc4e98a
Eng-abdalgabar <abood.alhemedi12@gmail.com>
2026-03-31 21:30:06 +0300

    0- stock

--- server/server.js
+++ server/server.js
@@ -5,15 +5,10 @@ import connectDB from './config/db.js';
 import userRouter from './routes/userRoute.js';
 import resumeRouter from './routes/resumeRouter.js';
 import aiRouter from './routes/aiRoute.js';
-import path from 'path';
-import { fileURLToPath } from "url";
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-// 🔥 حل مشكلة __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // database connection
 await connectDB();
@@ -24,17 +19,13 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-// API Routes
+// Routes
 app.use('/api/users', userRouter);
-app.use('/api/resume', resumeRouter);
-app.use('/api/ai', aiRouter);
 
-// 🔥 React build
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use('/api/resume',resumeRouter)
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
+app.use('/api/ai',aiRouter)
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
