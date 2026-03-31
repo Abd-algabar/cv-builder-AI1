2543285c6b42bd160a76407728a6f35881651f19
Eng-abdalgabar <abood.alhemedi12@gmail.com>
2026-03-31 21:26:26 +0300

    3-fix route

--- server/server.js
+++ server/server.js
@@ -6,9 +6,14 @@ import userRouter from './routes/userRoute.js';
 import resumeRouter from './routes/resumeRouter.js';
 import aiRouter from './routes/aiRoute.js';
 import path from 'path';
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// 🔥 حل مشكلة __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // database connection
 await connectDB();
@@ -19,19 +24,14 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-// Routes
+// API Routes
 app.use('/api/users', userRouter);
+app.use('/api/resume', resumeRouter);
+app.use('/api/ai', aiRouter);
 
-app.use('/api/resume',resumeRouter)
-
-app.use('/api/ai',aiRouter)
-
-// const path = require("path");
-
-// تقديم ملفات React
+// 🔥 React build
 app.use(express.static(path.join(__dirname, "client/build")));
 
-// ⚠️ هذا لازم يكون بعد كل API routes
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
